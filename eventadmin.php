2b23d048abc588d36ffbd7f337c1b4d8d7dafa1a
Pn chandramohan <42551385+iamcm1401@users.noreply.github.com>
2019-11-18 00:36:13 +0530

    Add files via upload

---
+++ eventadmin.php
@@ -0,0 +1,138 @@
+
+<?php
+
+?>
+<html>
+      
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+         <style>
+               .btn-group .button {
+                
+
+               background-color: #4CAF50; /* Green */
+                border: none;
+                color: white;
+                padding: 10px 32px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                cursor: pointer;
+                float: right;
+ }
+ 
+ .btn-group .button:hover {
+   background-color: #3e8e41;
+ }
+ table {
+   border-collapse: collapse;
+   width: 100%;
+ }
+ 
+ th, td {
+   text-align: left;
+   padding: 8px;
+ }
+ 
+ tr:nth-child(even){background-color: #f2f2f2}
+ 
+ th {
+   background-color:red;
+   color: white;
+ }
+ </style>
+        
+        <div class="btn-group">
+   <button class="button">registred</button>
+   <button class="button">winners</button>
+   <button class="button">logout</button>
+   <button class="button">recent</button>
+ </div>
+        
+      <center>WELCOME TO WEBSITE</center>
+      
+       
+     
+      
+       
+      
+       
+      </head>
+      <body>
+     
+       
+      <p align="center">Admin</p> 
+      
+       
+      <table width="10%" align="center" cellspacing="10" cellpadding="9" border="5">
+       
+      <tr>
+       
+      <th>id</th>
+       
+      <th>event</th>
+       
+      <th>date</th>
+      <th>college</th>
+      
+       
+      </tr>
+       
+      <tr>
+       
+      <td contenteditable="true">hi..fefem/.,/</td>      
+      <td contenteditable="true">sss</td>      
+      <td  contenteditable="true"></td>
+      <td  contenteditable="true"></td>
+      
+ 
+       
+      </tr>
+       
+      <tr>
+       
+      <td contenteditable="true">hhhh</td>      
+      <td contenteditable="true"></td>      
+      <td contenteditable="true"></td>
+      <td contenteditable="true"></td>
+      
+       
+      </tr>
+       
+      <tr>
+       
+      <td  contenteditable="true">Editable</td>      
+      <td  contenteditable="true">dd</td>      
+      <td contenteditable="true"></td>
+      <td contenteditable="true"></td>
+    
+       
+      </tr>
+ 
+      <tr>
+       
+       <td contenteditable="true">f</td>       
+       <td contenteditable="true"></td>      
+       <td contenteditable="true"></td>
+       <td contenteditable="true"></td>
+       
+        
+       </tr>
+ 
+       <tr>
+       
+       <td contenteditable="true">y</td> 
+       <td contenteditable="true"></td>  
+       <td contenteditable="true"></td>
+       <td contenteditable="true"></td>
+       
+        
+       </tr>
+       
+      </table>
+       
+      </body>
+       
+      </html>
